a77c0d153f3f477ba2726b7f7a9da310ebf96df0
Swampi995 <swp.mrs@gmail.com>
2018-03-11 18:31:33 +0200

    Initial Commit

---
+++ src/components/about/About.js
@@ -0,0 +1,12 @@
+/**
+ * Created by swpmr on 3/11/2018.
+ */
+import React from 'react'
+
+const About = () => (
+  <div>
+    <h1>Welcome to the Tornadoes Website!</h1>
+  </div>
+)
+
+export default About;
---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../main/Main'
+import Header from '../header/Header'
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
+
+export default App
