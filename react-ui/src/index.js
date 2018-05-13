7c7e24c2aac4be305538ad6ce50db24d475fa74f
Swampi995 <swp.mrs@gmail.com>
2018-05-13 16:51:34 +0300

    Add Redux Store

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app/App'
-import {BrowserRouter} from 'react-router-dom'
-import {MuiThemeProvider} from "material-ui";
+import { BrowserRouter } from 'react-router-dom'
+import { MuiThemeProvider } from 'material-ui'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer)
 
 ReactDOM.render((
-    <BrowserRouter>
-        <MuiThemeProvider>
-            <App/>
-        </MuiThemeProvider>
-    </BrowserRouter>
-), document.getElementById('root'));
+    <Provider store={store}>
+        <BrowserRouter>
+            <MuiThemeProvider>
+                <App/>
+            </MuiThemeProvider>
+        </BrowserRouter>
+    </Provider>
+), document.getElementById('root'))
---
+++ react-ui/src/reducers/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by swpmr on 5/13/2018.
+ */
+import { combineReducers } from 'redux'
+import todos from './todos'
+
+export default combineReducers({
+    todos
+})
